e7215cae87adc8b0498b1ad071175ba4194de214
Ashwini <ashwinipatil@gmail.com>
2023-04-24 14:22:44 +0530

    [MODIFIED]:[Ashwini] Rolling Die for Next Position

--- src/com/infotech/snakeandladder/SankeAndLadder.java
+++ src/com/infotech/snakeandladder/SankeAndLadder.java
@@ -1,12 +1,24 @@
 package com.infotech.snakeandladder;
 
+import java.util.Random;
+
 public class SankeAndLadder {
 	/** Defining Player Position in Game**/
 
+
 	public static void main(String[] args) {
 	int position=0;
+	/** Random class object to generate die No 1-6**/
+	Random random = new Random();
+	
 	System.out.println("Player first Position="+position);
-
+	
+    int dieno = random.nextInt(6)+1;
+    System.out.println("Die Number="+dieno);
+    
+    position = position + dieno;
+    
+    System.out.println("New Position after Rolling die="+position);
 	}
 
 }
