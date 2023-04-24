a83a7fcf6dd275ca392e20d53552ddbe68ae5dea
Ashwini <ashwinipatil@gmail.com>
2023-04-24 13:11:48 +0530

    [ADDED]:[AShwini] Snake and Ladder Project setup with Initial Position

---
+++ src/com/infotech/snakeandladder/SankeAndLadder.java
@@ -0,0 +1,12 @@
+package com.infotech.snakeandladder;
+
+public class SankeAndLadder {
+	/** Defining Player Position in Game**/
+
+	public static void main(String[] args) {
+	int position=0;
+	System.out.println("Player first Position="+position);
+
+	}
+
+}
